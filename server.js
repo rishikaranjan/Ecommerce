48b9aee563416c9f3354870e8e19c35668e8ba30
rishikaranjan <48781953+rishikaranjan@users.noreply.github.com>
2023-03-10 16:36:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from 'dotenv';
-import data from '../Data.js';
+
 
 import seedRouter from './Routes/seedRoutes.js';
 import productRouter from './Routes/productRoutes.js';
